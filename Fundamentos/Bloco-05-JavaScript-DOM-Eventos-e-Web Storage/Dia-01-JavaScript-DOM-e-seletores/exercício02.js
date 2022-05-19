695c568f005e41fce53f44684d79976d495dc5d8
Luiz Filipe <luizfilipelgs@gmail.com>
2022-05-19 20:52:14 -0300

    atividade 2

---
+++ Fundamentos/Bloco-05-JavaScript-DOM-Eventos-e-Web Storage/Dia-01-JavaScript-DOM-e-seletores/exercício02.js
@@ -0,0 +1,16 @@
+document.getElementById("header-container").style.backgroundColor = "green"
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "red"
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow"
+
+document.getElementsByClassName("urgente")[0].style.backgroundColor = "black"
+document.getElementsByClassName("urgente")[1].style.backgroundColor = "black"
+
+document.querySelectorAll(".naoUrgente")[0].style.backgroundColor = "green"
+document.querySelectorAll(".naoUrgente")[1].style.backgroundColor = "green"
+
+let h3 = document.getElementsByTagName("h3")
+for (let index = 0; index < h3.length; index++) {
+    h3[index].style.color = "white"
+    
+}
+document.querySelector("#footer-container").style.backgroundColor = "black"
